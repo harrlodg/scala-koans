21d6aba67510b049ebd0cfe5bcd9eb2fd1fd0305
Harry Lodge <harry.lodge@itv.com>
2023-05-31 12:39:55 +0100

    koans completed

--- src/test/scala/org/scalakoans/AboutLiteralBooleans.scala
+++ src/test/scala/org/scalakoans/AboutLiteralBooleans.scala
@@ -1,24 +1,25 @@
 package org.scalakoans
 
-
 import support.KoanSuite
 import support.BlankValues.__
 
-class AboutLiteralBooleans extends KoanSuite  {
+class AboutLiteralBooleans extends KoanSuite {
 
-  koan("""Boolean literals are either true or false, using the true or false keyword""") {
+  koan(
+    """Boolean literals are either true or false, using the true or false keyword"""
+  ) {
     val a = true
     val b = false
     val c = 1 > 2
     val d = 1 < 2
     val e = a == c
     val f = b == d
-    a should be(__)
-    b should be(__)
-    c should be(__)
-    d should be(__)
-    e should be(__)
-    f should be(__)
+    a should be(true)
+    b should be(false)
+    c should be(false)
+    d should be(true)
+    e should be(false)
+    f should be(false)
   }
 
 }
--- src/test/scala/org/scalakoans/AboutValAndVar.scala
+++ src/test/scala/org/scalakoans/AboutValAndVar.scala
@@ -3,28 +3,29 @@ package org.scalakoans
 import support.KoanSuite
 import support.BlankValues.__
 
-class AboutValAndVar extends KoanSuite  {
+class AboutValAndVar extends KoanSuite {
   koan("vars may be reassigned") {
     var a = 5
-    a should be(__)
+    a should be(5)
 
     a = 7
-    a should be(__)
+    a should be(7)
   }
 
   koan("vals may not be reassigned") {
     val a = 5
-    a should be(__)
+    a should be(5)
 
     // What happens if you uncomment these lines?
     // a = 7
-    // a should be (5)
+    // a should be(5)
   }
 
-  koan("vals or vars can have the same name as a keyword as long as it's surrounded by `") {
+  koan(
+    "vals or vars can have the same name as a keyword as long as it's surrounded by `"
+  ) {
     val `class` = "MyClassName"
-    `class`.shouldBe(__)
+    `class`.shouldBe("MyClassName")
   }
-
 
 }
