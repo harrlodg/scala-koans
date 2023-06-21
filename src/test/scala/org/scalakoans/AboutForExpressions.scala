796b8431fbfd93c116f5bedfad76c643808b7983
Harry Lodge <harry.lodge@itv.com>
2023-06-21 11:13:45 +0100

    koan complete

--- src/test/scala/org/scalakoans/AboutForExpressions.scala
+++ src/test/scala/org/scalakoans/AboutForExpressions.scala
@@ -3,8 +3,7 @@ package org.scalakoans
 import support.KoanSuite
 import support.BlankValues._
 
-
-class AboutForExpressions extends KoanSuite  {
+class AboutForExpressions extends KoanSuite {
 
   koan("For loops can be simple") {
     val someNumbers = Range(0, 10)
@@ -12,7 +11,7 @@ class AboutForExpressions extends KoanSuite  {
     for (i <- someNumbers)
       sum += i
 
-    sum should equal(__)
+    sum should equal(45)
   }
 
   koan("For loops can contain additional logic") {
@@ -22,7 +21,7 @@ class AboutForExpressions extends KoanSuite  {
     for (i <- someNumbers)
       if (i % 2 == 0) sum += i
 
-    sum should equal(__)
+    sum should equal(20)
   }
   koan("For loops can produce a list which can be summed easily") {
     val someNumbers = Range(0, 10)
@@ -31,22 +30,27 @@ class AboutForExpressions extends KoanSuite  {
       for {
         i <- someNumbers
         if ((i % 2) == 0)
-      }
-      yield i
+      } yield i
 
-    theList.reduceLeft(_ + _) should be(__)
+    theList.reduceLeft(_ + _) should be(20)
   }
 
-  koan("For expressions can nest, with later generators varying more rapidly than earlier ones") {
+  koan(
+    "For expressions can nest, with later generators varying more rapidly than earlier ones"
+  ) {
     val xValues = Range(1, 5)
     val yValues = Range(1, 3)
     val coordinates = for {
       x <- xValues
       y <- yValues
-    }
-    yield (x, y)
-    coordinates(4) should be(__)
+    } yield (x, y)
+    coordinates(4) should be((3, 1))
   }
-
+  // https://alvinalexander.com/scala/how-to-use-multiple-generators-scala-for-expressions-loops/
+  // 1,1
+  // 1,2
+  // 2,1
+  // 2,2
+  // 3,1
 
 }
--- src/test/scala/org/scalakoans/AboutHigherOrderFunctions.scala
+++ src/test/scala/org/scalakoans/AboutHigherOrderFunctions.scala
@@ -5,20 +5,24 @@ import support.BlankValues._
 
 import collection.immutable.List
 
-class AboutHigherOrderFunctions extends KoanSuite  {
+class AboutHigherOrderFunctions extends KoanSuite {
 
   koan("A variable referencing an anonymous function") {
-    val lambda = {
-      x: Int => x + 1
+    val lambda = { x: Int =>
+      x + 1
     }
-    def result = List(1, 2, 3) map lambda //FYI: map runs a function on each element of a collection
-    result should be(__)
+    def result = List(
+      1,
+      2,
+      3
+    ) map lambda // FYI: map runs a function on each element of a collection
+    result should be(List(2, 3, 4))
   }
 
   koan("Another way for a variable to reference an anonymous function") {
     val lambda = (x: Int) => x + 1
     def result = List(1, 2, 3) map lambda
-    result should be(__)
+    result should be(List(2, 3, 4))
   }
 
   koan("Declaring a variable to reference an explicitly created function") {
@@ -26,32 +30,34 @@ class AboutHigherOrderFunctions extends KoanSuite  {
       def apply(v1: Int) = v1 + 1
     }
     def result = List(1, 2, 3) map lambda
-    result should be(__)
+    result should be(List(2, 3, 4))
   }
 
-  koan("""Another way for a variable to reference an explicitly created function.  In this case (Int) => Int
-          | is the same type as Function1[Int, Int]""") {
+  koan(
+    """Another way for a variable to reference an explicitly created function.  In this case (Int) => Int
+          | is the same type as Function1[Int, Int]"""
+  ) {
     val lambda = new ((Int) => Int) {
       def apply(v1: Int) = v1 + 1
     }
     def result = List(1, 2, 3) map lambda
-    result should be(__)
+    result should be(List(2, 3, 4))
   }
 
   koan("A closure is any function that works with it's surroundings") {
     var incrementor = 1
 
-    def closure = {
-      x: Int => x + incrementor
+    def closure = { x: Int =>
+      x + incrementor
     }
 
     val result = List(1, 2, 3) map closure
-    result should be(__)
+    result should be(List(2, 3, 4))
 
     incrementor = 2
 
     val result1 = List(1, 2, 3) map closure
-    result1 should be(__)
+    result1 should be(List(3, 4, 5))
   }
 
   koan("A function returning another function") {
@@ -61,51 +67,59 @@ class AboutHigherOrderFunctions extends KoanSuite  {
       }
     }
 
-    addWithoutSyntaxSugar(1)(2) should be(__)
+    addWithoutSyntaxSugar(1)(2) should be(3)
 
     def add(x: Int) = (y: Int) => x + y
-    add(2)(3) should be(__)
+    add(2)(3) should be(5)
 
     def fiveAdder = add(5)
-    fiveAdder(5) should be(__)
+    fiveAdder(5) should be(10)
   }
 
-
-  koan("function taking another function as parameter. Helps in compositioning functions") {
+  koan(
+    "function taking another function as parameter. Helps in compositioning functions"
+  ) {
     def makeUpper(xs: List[String]) = xs map {
       _.toUpperCase
     }
     def makeWhatEverYouLike(xs: List[String], sideEffect: String => String) = {
       xs map sideEffect
     }
-    makeUpper(List("abc", "xyz", "123")) should be(__)
+    makeUpper(List("abc", "xyz", "123")) should be(List("ABC", "XYZ", "123"))
 
-    makeWhatEverYouLike(List("ABC", "XYZ", "123"), {
-      x => x.toLowerCase
-    }) should be(__)
-    //using it inline
+    makeWhatEverYouLike(
+      List("ABC", "XYZ", "123"),
+      { x =>
+        x.toLowerCase
+      }
+    ) should be(List("abc", "xyz", "123"))
+    // using it inline
     List("Scala", "Erlang", "Clojure") map {
       _.length
-    } should be(__)
+    } should be(List(5, 6, 7))
   }
 
-  koan("Currying is a technique to transform function with multiple parameters to function with one parameter") {
+  koan(
+    "Currying is a technique to transform function with multiple parameters to function with one parameter"
+  ) {
     def multiply(x: Int, y: Int) = x * y
     val multiplyCurried = (multiply _).curried
-    multiply(4, 5) should be(__)
-    multiplyCurried(3)(2) should be(__)
+    multiply(4, 5) should be(20)
+    multiplyCurried(3)(2) should be(6)
   }
 
-  koan("Currying allows you to create specialized version of generalized function") {
+  koan(
+    "Currying allows you to create specialized version of generalized function"
+  ) {
     def customFilter(f: Int => Boolean)(xs: List[Int]) = {
       xs filter f
     }
     def onlyEven(x: Int) = x % 2 == 0
     val xs = List(12, 11, 5, 20, 3, 13, 2)
-    customFilter(onlyEven)(xs) should be(__)
+    customFilter(onlyEven)(xs) should be(List(12, 20, 2))
 
     val onlyEvenFilter = customFilter(onlyEven) _
-    onlyEvenFilter(xs) should be(__)
+    onlyEvenFilter(xs) should be(List(12, 20, 2))
   }
 
   koan("Using a method inside a class to create a function") {
@@ -124,6 +138,9 @@ class AboutHigherOrderFunctions extends KoanSuite  {
     val enchantress = new PokerTable("Enchantress", 15, Nil)
 
     val x = theTexas.isAvailable _
-    List(12, 2, 3, 4, 5, 6, 11, 44).filter(x)
+    assert(List(12, 11, 44) === List(12, 2, 3, 4, 5, 6, 11, 44).filter(x))
+    // ???
+    // surely it makes more sense to return the values where the amount is less than
+    // the capacity?
   }
 }
